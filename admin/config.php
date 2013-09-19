bd463bf54e2e3f43a04c7b02075f186219f528aa
Muhammad Fauzi <fauzieuy@gmail.com>
2013-09-19 20:13:38 +0700

    commit

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/testshop/admin/');
+define('HTTP_CATALOG', 'http://localhost/testshop/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/testshop/admin/');
+define('HTTPS_CATALOG', 'http://localhost/testshop/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\wamp\www\testshop/admin/');
+define('DIR_SYSTEM', 'C:\wamp\www\testshop/system/');
+define('DIR_DATABASE', 'C:\wamp\www\testshop/system/database/');
+define('DIR_LANGUAGE', 'C:\wamp\www\testshop/admin/language/');
+define('DIR_TEMPLATE', 'C:\wamp\www\testshop/admin/view/template/');
+define('DIR_CONFIG', 'C:\wamp\www\testshop/system/config/');
+define('DIR_IMAGE', 'C:\wamp\www\testshop/image/');
+define('DIR_CACHE', 'C:\wamp\www\testshop/system/cache/');
+define('DIR_DOWNLOAD', 'C:\wamp\www\testshop/download/');
+define('DIR_LOGS', 'C:\wamp\www\testshop/system/logs/');
+define('DIR_CATALOG', 'C:\wamp\www\testshop/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'testshop');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/testshop/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/testshop/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\wamp\www\testshop/catalog/');
+define('DIR_SYSTEM', 'C:\wamp\www\testshop/system/');
+define('DIR_DATABASE', 'C:\wamp\www\testshop/system/database/');
+define('DIR_LANGUAGE', 'C:\wamp\www\testshop/catalog/language/');
+define('DIR_TEMPLATE', 'C:\wamp\www\testshop/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\wamp\www\testshop/system/config/');
+define('DIR_IMAGE', 'C:\wamp\www\testshop/image/');
+define('DIR_CACHE', 'C:\wamp\www\testshop/system/cache/');
+define('DIR_DOWNLOAD', 'C:\wamp\www\testshop/download/');
+define('DIR_LOGS', 'C:\wamp\www\testshop/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'testshop');
+define('DB_PREFIX', 'oc_');
+?>
